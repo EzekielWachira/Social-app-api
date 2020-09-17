fe930c8488cb77d63667b360dbfadc731afbb6e3
Ezekiel Wachira <ezekielwachira048@gmail.com>
2020-09-17 12:04:17 +0300

    Added Routes for posts and likes, PostController and LikesController functionality

--- app/Http/Controllers/LikeController.php
+++ app/Http/Controllers/LikeController.php
@@ -2,9 +2,32 @@
 
 namespace App\Http\Controllers;
 
+use App\Comment;
+use App\Http\Resources\LikesResource;
+use App\Like;
+use App\Post;
 use Illuminate\Http\Request;
 
 class LikeController extends Controller
 {
-    //
+    public function index() {
+        $like = Like::with('user', 'post')->orderByDesc('created_at')
+            ->paginate(20);
+        return LikesResource::collection($like);
+    }
+
+    public function store(Request $request, Post $post){
+        $like = new Like();
+        $like->user_id = $request->user()->id;
+        $like->post_id = $post->id;
+        $like->save();
+        return new LikesResource($like);
+    }
+
+    public function delete(Like $like){
+        $like->delete();
+        return response([
+            'message' => 'User Unliked the post'
+        ]);
+    }
 }
